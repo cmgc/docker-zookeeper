937933110393c8a7be3827a8b398fec1a9e3e77e
cmgc <unlesnick@gmail.com>
2015-07-07 13:08:57 +0300

    rename variable in zk_clean_job.sh.

--- zookeeper-3.4/zk_clean_job.sh
+++ zookeeper-3.4/zk_clean_job.sh
@@ -2,13 +2,12 @@
 
 ADDED="$(crontab -l | grep zkCleanup.sh | wc -l)"
 
-if [ "$ADDEDD" != "0" ]; then
+if [ "$ADDED" != "0" ]; then
 	exit;
 fi
 
 # workaround for zkCleanup.sh. We need cleanup only snapshots /var/lib/zookeper
 sed -i '/ZOODATALOGDIR/ s/^/#/' /opt/zookeeper/bin/zkCleanup.sh
-
 # Daily cleanup. After cleanup will remain only 3 last snapshots
 crontab -l | { cat; echo "0	0	*	*	*	/opt/zookeeper/bin/zkCleanup.sh -n 3"; echo ; } | crontab -
 
